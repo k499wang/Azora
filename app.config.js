67c1bd6ee55bd1662963aedc25c0892195d7f8a7
kevin wang <k3vinwvng@gmail.com>
2026-05-21 19:59:07 -0400

    Build Changes

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'Azora',
     slug: 'Azora',
-    version: '1.0.0',
+    version: '1.0.1',
     orientation: 'portrait',
     icon: './assets/iconApp.png',
     userInterfaceStyle: 'light',
